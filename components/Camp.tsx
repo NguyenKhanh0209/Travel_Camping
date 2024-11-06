e0cc2570da4a6748eb6ea6c71e70d14e099848c2
NguyenKhanh0209 <nguyentranquockhanh294@gmail.com>
2024-11-06 21:00:34 +0700

    update

--- components/Camp.tsx
+++ components/Camp.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
-const Camp = () => {
-  return (
-    <div>Camp</div>
-  )
+interface CampProps {
+  backgroundImage: string;
+  title: string;
+  subtitle: string;
+  peopleJoined: string;
 }
 
-export default Camp
+const CampSite = ({
+  backgroundImage,
+  title,
+  subtitle,
+  peopleJoined,
+}: CampProps) => {
+  return <div>Camp site 1</div>;
+};
+const Camp = () => {
+  return (
+    <section className="border-2 border-green-400 2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
+      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
+        {/* <CampSite backgroundImage=""/>
+        <CampSite /> */}
+      </div>
+    </section>
+  );
+};
+
+export default Camp;
